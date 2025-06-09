4fe86abe666a43444f6dea7fbedfa63851dd3ad3
Anshu Mishra <anshumishraocog@gmail.com>
2025-06-09 19:09:11 +0530

    Altered home component to show fileupload and about

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Button } from './ui/button';
 import { useNavigate } from 'react-router-dom';
+import About from './About';
+import FileUpload from './FileUpload';
 
 const Home = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="relative w-full min-h-screen bg-white">
+    <div className="relative w-full min-h-screen flex flex-col gap-2 bg-white">
       <div className="absolute top-4 right-4">
-    <Button onClick={()=>navigate('/auth')} variant="outline">Auth</Button>
+    <Button onClick={()=>navigate('/auth')} variant="outline">Sign Up/Log In</Button>
       </div>
-
-      <div className="flex justify-center items-center min-h-screen w-full">
-        <h1 className="text-3xl font-bold text-black">This is homepage</h1>
-      </div>
+     <About></About>
+     <FileUpload></FileUpload>
     </div>
   );
 };
